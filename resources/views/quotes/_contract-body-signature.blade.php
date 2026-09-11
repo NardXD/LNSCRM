8db71a8e92af943108b24ab53a955129d19b0a8a
nard00261 <120164934+nard00261@users.noreply.github.com>
2026-09-11 08:20:05 +0800

    signature and date in contract - Removed the underline wherever a real value now sits

--- resources/views/quotes/_contract-body-signature.blade.php
+++ resources/views/quotes/_contract-body-signature.blade.php
@@ -4,16 +4,23 @@
             <td style="width:44%">
                 @if ($data['signature_base64'] ?? null)
                     <img class="signature-img" src="data:image/png;base64,{{ $data['signature_base64'] }}">
+                @else
+                    <span class="signature-line" style="width:100%;">&nbsp;</span>
                 @endif
-                <span class="signature-line" style="width:100%;">&nbsp;</span>
             </td>
             <td style="width:8%; white-space:nowrap;">Date :</td>
-            <td>{{ $data['signature_signed_at'] ?? '' }} <span class="signature-line" style="width:100%;">&nbsp;</span></td>
+            <td>
+                @if ($data['signature_signed_at'] ?? null)
+                    {{ $data['signature_signed_at'] }}
+                @else
+                    <span class="signature-line" style="width:100%;">&nbsp;</span>
+                @endif
+            </td>
         </tr>
         <tr>
             <td><strong>Loc&amp;Stor 24/7, Inc:</strong></td>
             <td><span class="signature-line" style="width:100%;">&nbsp;</span></td>
             <td style="white-space:nowrap;">Date :</td>
-            <td>{{ $data['generated_at'] }} <span class="signature-line" style="width:60%;">&nbsp;</span></td>
+            <td>{{ $data['generated_at'] }}</td>
         </tr>
     </table>
